953f014eea58fb7a09950ec360c5b23aae8d74b4
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-05-10 21:25:01 +0700

    Add More Translations

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Internationalization Extension for Mecha
 Release Notes
 -------------
 
-### 1.0.1
+### 1.0.1, 1.0.2
 
  - Added more translation items.
 
--- language/state/en.php
+++ language/state/en.php
@@ -4,6 +4,9 @@
 
 return [
 
+    // Common
+    'Loading...' => 'Loading…',
+
     // Sentence(s)
     'This value does not determine the I18N system on your site unless you want to make an I18N extension that depends on this value.' => 'You have language extension installed. This value now determines the I18N system on your site.'
 
