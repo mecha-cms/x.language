64e3eb7dcd059ebb90bcf04a91f04e477e26674f
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-05-18 19:43:25 +0700

    Update

--- language/state/en.php
+++ language/state/en.php
@@ -4,8 +4,10 @@
 
 return [
 
-    // Common
-    'Loading...' => 'Loading…',
+    // Action(s)
+    'Loading...' => "Loading\u{2026}",
+    'Searching...' => "Searching\u{2026}",
+    'Uploading...' => "Uploading\u{2026}",
 
     // Sentence(s)
     'This value does not determine the I18N system on your site unless you want to make an I18N extension that depends on this value.' => 'You have language extension installed. This value now determines the I18N system on your site.'
